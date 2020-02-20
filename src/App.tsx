466e4970dc07599e74952c4634dc4ffe3cb475aa
serhan iskander <serhan.i@taboola.com>
2020-02-20 19:44:44 +0200

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,9 @@
 import React, {useState} from 'react';
+import {updateGrid, getGridState} from "./Utils";
 import './App.css';
 
 function App() {
-
-    function getState(): Array<Array<Boolean>> {
-        const grid: Array<Array<Boolean>> = [];
-        for (let i: number = 0; i < 50; i++) {
-            grid[i] = [];
-            for (let j: number = 0; j < 50; j++) {
-                grid[i][j] = Math.random() >= 0.5;
-            }
-        }
-        return grid;
-    }
-
-    function getStatus(gridElementElement: Boolean, liveNeighbours: number) {
-        if (gridElementElement) {
-            if (liveNeighbours < 2) {
-                return false;
-            }
-            return liveNeighbours <= 3;
-
-        }
-        return liveNeighbours === 3;
-    }
-
-    function updateGrid(grid: Array<Array<Boolean>>, setGrid: (value: (((prevState: Array<Array<Boolean>>) => Array<Array<Boolean>>) | Array<Array<Boolean>>)) => void) {
-        const newGrid: Array<Array<Boolean>> = [];
-        grid.forEach((row: Boolean[], i: number) => {
-            row.forEach((val: Boolean, j: number) => {
-                let liveNeighbours: number = 0;
-                if (i > 0) {
-                    grid[i - 1][j] && liveNeighbours++;
-                    if (j > 0) {
-                        grid[i - 1][j - 1] && liveNeighbours++;
-                    }
-                    if (j < 49) {
-                        grid[i - 1][j + 1] && liveNeighbours++;
-                    }
-                }
-                if (j > 0) {
-                    grid[i][j - 1] && liveNeighbours++;
-                    if (i < 49) {
-                        grid[i + 1][j - 1] && liveNeighbours++;
-                    }
-                }
-                if (i < 49) {
-                    grid[i + 1][j] && liveNeighbours++;
-                    if (j < 49) {
-                        grid[i + 1][j + 1] && liveNeighbours++;
-                    }
-                }
-                if (j < 49) {
-                    grid[i][j + 1] && liveNeighbours++;
-                }
-                newGrid[i] = newGrid[i] || [];
-                console.log("updating");
-                debugger;
-                newGrid[i][j] = getStatus(grid[i][j], liveNeighbours);
-            })
-        });
-        setGrid(newGrid);
-    }
-
-    const [grid, setGrid] = useState(getState());
+    const [grid, setGrid] = useState(getGridState());
 
     const gridElements = grid.map((row: Array<Boolean>, i: number) => {
         const rows = row.map((val: Boolean, j: number) => {
---
+++ src/Utils.ts
@@ -0,0 +1,62 @@
+export function updateGrid(grid: Array<Array<Boolean>>, setGrid: (value: (((prevState: Array<Array<Boolean>>) => Array<Array<Boolean>>) | Array<Array<Boolean>>)) => void) {
+    const newGrid: Array<Array<Boolean>> = [];
+    grid.forEach((row: Boolean[], i: number) => {
+        row.forEach((val: Boolean, j: number) => {
+            let liveNeighbours: number = 0;
+            if (i > 0) {
+                grid[i - 1][j] && liveNeighbours++;
+                if (j > 0) {
+                    grid[i - 1][j - 1] && liveNeighbours++;
+                }
+                if (j < 49) {
+                    grid[i - 1][j + 1] && liveNeighbours++;
+                }
+            }
+            if (j > 0) {
+                grid[i][j - 1] && liveNeighbours++;
+                if (i < 49) {
+                    grid[i + 1][j - 1] && liveNeighbours++;
+                }
+            }
+            if (i < 49) {
+                grid[i + 1][j] && liveNeighbours++;
+                if (j < 49) {
+                    grid[i + 1][j + 1] && liveNeighbours++;
+                }
+            }
+            if (j < 49) {
+                grid[i][j + 1] && liveNeighbours++;
+            }
+            newGrid[i] = newGrid[i] || [];
+            console.log("updating");
+            debugger;
+            newGrid[i][j] = getStatus(grid[i][j], liveNeighbours);
+        })
+    });
+    setGrid(newGrid);
+}
+
+function getStatus(gridElementElement: Boolean, liveNeighbours: number) {
+    if (gridElementElement) {
+        if (liveNeighbours < 2) {
+            return false;
+        }
+        return liveNeighbours <= 3;
+
+    }
+    return liveNeighbours === 3;
+}
+
+export function getGridState(): Array<Array<Boolean>> {
+    const grid: Array<Array<Boolean>> = [];
+    for (let i: number = 0; i < 50; i++) {
+        grid[i] = [];
+        for (let j: number = 0; j < 50; j++) {
+            grid[i][j] = Math.random() >= 0.5;
+        }
+    }
+    return grid;
+}
+
+
+
